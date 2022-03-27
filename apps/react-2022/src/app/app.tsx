4122bf8c5b2c829e988ad28a245ec317f149dcab
Chandra Sekhar Veera <veeracs@gmail.com>
2022-03-27 12:28:08 -0400

    initial commit

---
+++ apps/react-2022/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="react-2022" />
+    </StyledApp>
+  );
+}
+
+export default App;
